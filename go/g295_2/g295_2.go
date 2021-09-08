d4d667c6e98a7e122959026480e8d783cb2953f5
ruizhang <ruizhang@pateo.com.cn>
2021-09-08 18:06:50 +0800

    数据流的中位数

--- go/g295_2/g295_2.go
+++ go/g295_2/g295_2.go
@@ -1,35 +1,79 @@
+//数据流的中位数
 package g295_2
 
-import "sort"
+import (
+	"container/heap"
+	"sort"
+)
 
 type MedianFinder struct {
-	Data []int
+	max *MaxHeap
+	min *MinHeap
 }
 
 func Constructor() MedianFinder {
 	var medianFinder MedianFinder
-	medianFinder.Data = make([]int, 0)
+	medianFinder.max = &MaxHeap{}
+	medianFinder.min = &MinHeap{}
 	return medianFinder
 }
 
 func (this *MedianFinder) AddNum(num int) {
-	this.Data = append(this.Data, num)
 
+	if this.min.Len() == 0 || num <= this.min.IntSlice[0] {
+		heap.Push(this.min, num)
+		if this.max.Len()+1 < this.min.Len() {
+			heap.Push(this.max, heap.Pop(this.min))
+		}
+
+	} else {
+
+		heap.Push(this.max, num)
+
+		if this.max.Len() > this.min.Len() {
+			heap.Push(this.min, heap.Pop(this.max))
+		}
+
+	}
 }
 
 func (this *MedianFinder) FindMedian() float64 {
-	sort.Ints(this.Data)
-	size := len(this.Data)
-
-	if size&1 == 1 {
-		mid := (size + 1) / 2
-		result := this.Data[mid-1]
-		return float64(result)
-	} else {
-		mid := size / 2
-
-		result := float64(this.Data[mid-1]+this.Data[mid]) / 2.0
-		return float64(result)
+	if this.min.Len() == 0 {
+		return 0
 	}
 
+	if this.max.Len() == this.min.Len() {
+		return float64(this.min.IntSlice[0]+this.max.IntSlice[0]) / 2.0
+	}
+	return float64(this.min.IntSlice[0])
+}
+
+type MaxHeap struct {
+	MedianHeap
+}
+
+type MinHeap struct {
+	MedianHeap
+}
+
+type MedianHeap struct {
+	sort.IntSlice
+}
+
+func (mh *MedianHeap) Push(v interface{}) {
+	mh.IntSlice = append(mh.IntSlice, v.(int))
+}
+
+func (mh *MedianHeap) Pop() interface{} {
+	temp := mh.IntSlice
+	result := temp[len(temp)-1]
+	mh.IntSlice = temp[:len(temp)-1]
+	return result
+}
+
+func (max MaxHeap) Less(i, j int) bool {
+	return max.IntSlice[i] < max.IntSlice[j]
+}
+func (max MinHeap) Less(i, j int) bool {
+	return max.IntSlice[i] > max.IntSlice[j]
 }
