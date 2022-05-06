0d4abb4a430187e174b1097229d924e8f9ead0b3
ruizhang <ruizhang@pateo.com.cn>
2022-05-06 10:31:37 +0800

    g933 每日一题

---
+++ go/g933/g933.go
@@ -0,0 +1,43 @@
+package g933
+
+/**
+
+写一个 RecentCounter 类来计算特定时间范围内最近的请求。
+
+请你实现 RecentCounter 类：
+
+RecentCounter() 初始化计数器，请求数为 0 。
+int ping(int t) 在时间 t 添加一个新请求，其中 t 表示以毫秒为单位的某个时间，并返回过去 3000 毫秒内发生的所有请求数（包括新请求）。确切地说，返回在 [t-3000, t] 内发生的请求数。
+保证 每次对 ping 的调用都使用比之前更大的 t 值
+
+
+*/
+
+type RecentCounter struct {
+	request []int
+}
+
+func Constructor() RecentCounter {
+	return RecentCounter{
+		request: make([]int, 0),
+	}
+}
+
+func (this *RecentCounter) Ping(t int) int {
+	this.request = append(this.request, t)
+	start := t - 3000
+	r := 1
+	for i, v := range this.request {
+		if v >= start {
+			r = len(this.request) - i
+			break
+		}
+	}
+	return r
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * obj := Constructor();
+ * param_1 := obj.Ping(t);
+ */
