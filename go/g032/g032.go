d86079439d53363decce049dc88b57cceebcba79
ruizhang <ruizhang@pateo.com.cn>
2022-05-11 14:37:24 +0800

    整理文件夹

--- go/g032/g032.go
+++ go/g032/g032.go
@@ -1,31 +1,56 @@
-package g032
+package main
 
-/**
+//func longestValidParentheses(s string) int {
+//	len_s := len(s)
+//	temp := ""
+//	for j := len_s; j > 0; j-- {
+//		for i := 0; i <= len_s-j; i++ {
+//			temp = s[i : i+j]
+//			if isValid(temp) {
+//				return len(temp)
+//			}
+//		}
+//	}
+//	return 0
+//}
 
-给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
-
-请必须使用时间复杂度为 O(log n) 的算法
-
-
-*/
-
-func searchInsert(nums []int, target int) int {
-	left, right := 0, len(nums)-1
-
-	for left < right {
-		mid := left + (right-left)/2
-		if nums[mid] == target {
-			return mid
-		} else if nums[mid] < target {
-			left = mid + 1
-		} else {
-			right = mid - 1
+func longestValidParentheses(s string) int {
+	bytes := []byte(s)
+	if len(bytes) < 2 {
+		return 0
+	}
+	lengthList := make([]int, len(bytes))
+	var max int
+	for i := 1; i < len(bytes); i++ {
+		if bytes[i] == ')' {
+			j := i - lengthList[i-1] - 1
+			if j >= 0 && bytes[j] == '(' {
+				lengthList[i] = lengthList[i-1] + 2
+				if j-1 >= 0 {
+					lengthList[i] += lengthList[j-1]
+				}
+			}
+		}
+		if lengthList[i] > max {
+			max = lengthList[i]
 		}
 	}
-	if nums[left] >= target {
-		return left
-	} else {
-		return left + 1
-	}
+	return max
+}
 
+func isValid(str string) bool {
+	array := make([]byte, len(str)+1)
+	index := 1
+	for i := 0; i < len(str); i++ {
+		if str[i] == '(' {
+			array[index] = str[i]
+			index++
+		} else {
+			index--
+			if array[index] != '(' {
+				return false
+			}
+		}
+	}
+	return index == 1
 }
---
+++ go/g035/g035.go
@@ -0,0 +1,31 @@
+package g035
+
+/**
+
+给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+
+请必须使用时间复杂度为 O(log n) 的算法
+
+
+*/
+
+func searchInsert(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+
+	for left < right {
+		mid := left + (right-left)/2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < target {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+	if nums[left] >= target {
+		return left
+	} else {
+		return left + 1
+	}
+
+}
