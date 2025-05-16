52189febe87f2df614f24851f6d1eb41a43ce4b1
zhangrui49 <429835306@qq.com>
2025-05-16 18:14:40 +0800

    优化

--- go/g3337/g3337.go
+++ go/g3337/g3337.go
@@ -7,7 +7,70 @@ package g3337
  */
 
 // @lc code=start
+// 跑到 529/536 超时了
+
 func lengthAfterTransformations(s string, t int, nums []int) int {
+	total := 0
+	mod := int(1e9 + 7)
+	counts := make([]int, 26)
+	for _, v := range s {
+		counts[int(v-'a')]++
+	}
+
+	for i := 0; i < t; i++ {
+		newCounts := make([]int, 26)
+		for j := 0; j < 26; j++ {
+			if counts[j] == 0 {
+				continue
+			}
+			origin := counts[j]
+			move := nums[j]
+			for k := 1; k <= move; k++ {
+				d := (j + k) % 26
+				//这里提前取模，避免数字越界
+				newCounts[d] = (origin + newCounts[d]) % mod
+			}
+		}
+		counts = newCounts
+	}
+	for _, v := range counts {
+		total = (total + v) % mod
+	}
+	return total
+}
+
+// 通过计数优化内存，但是数字会越界
+func lengthAfterTransformationsOverSize(s string, t int, nums []int) int {
+	total := 0
+	counts := make([]int, 26)
+	for _, v := range s {
+		counts[int(v-'a')]++
+	}
+
+	for i := 0; i < t; i++ {
+		newCounts := make([]int, 26)
+		for j := 0; j < 26; j++ {
+			if counts[j] == 0 {
+				continue
+			}
+			origin := counts[j]
+			move := nums[j]
+			for k := 1; k <= move; k++ {
+				d := (j + k) % 26
+				newCounts[d] += origin
+			}
+		}
+		counts = newCounts
+	}
+	for _, v := range counts {
+		total += v
+	}
+	total = total % (1e9 + 7)
+	return total
+}
+
+// 暴力法，内存会超标
+func lengthAfterTransformationsLowMem(s string, t int, nums []int) int {
 	total := 0
 	trans := make([]int, len(s))
 	for i, v := range s {
